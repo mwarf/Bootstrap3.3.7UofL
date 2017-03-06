2f786050f6a7d751c69301bad88582388125f086
Michael Warf <devbox@gmail.com>
2017-03-06 07:00:25 -0700

    Committing Changes - first commit

---
+++ parts/shared/footer.php
@@ -0,0 +1,92 @@
+	
+<div class="footer">
+	<div id="unit-footer" class="container-fluid">
+		<div  class="container">
+			<div class="row"><hr class="my-4"></div>
+			<div class="row">
+				<div class="col-md-4">
+
+  <ul>
+	  <li><strong>University Advancement - Communications</strong></li>
+  <li><span class="glyphicon glyphicon-map-marker"></span>A735 (University Hall)</li>
+  <li><span class="glyphicon glyphicon-phone"></span> (403) 329-2582</li>
+  <li><span class="glyphicon glyphicon-print"></span> (403) 329-2211</li>
+  <li><span class="glyphicon glyphicon-envelope"></span> <a href="mailto:communications@ulethbridge.ca">communications@ulethbridge.ca</a></li>
+  </ul>
+
+				</div>
+				<div class="col-md-4">
+					<ul>
+					<li><strong>Social Media</strong></li>
+					<li><a href="#"><i class="fa fa-facebook-official"></i> ulethbridge.ca </li></a>
+					<li><a href="#"><i class="fa fa-twitter"></i> @ulethbridge.ca </li></a>
+					<li><a href="#"><i class="fa fa-instagram"></i> @ulethbridge </li></a>
+					<li><a href="#"><i class="fa fa-linkedin-square"></i> ulethbridge </li></a>
+					
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<ul>
+					<li><strong>Related Links</strong></li>
+					<li><a href="#">UNews</a></li>
+					<li><a href="#">Government Relations</a></li>
+					<li><a href="#">Alumni Relations</a></li>
+					<li><a href="#">Giving</a></li>
+					</ul>				
+					</div>
+			</div>
+		</div>
+	</div>
+	<div id="global-footer" class="container-fluid">
+		<div  class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<ul>
+					<li><strong>Connect</strong></li>
+					<li><a href="#"><i class="fa fa-facebook-official"></i> ulethbridge.ca </li></a>
+					<li><a href="#"><i class="fa fa-twitter"></i> @ulethbridge.ca </li></a>
+					<li><a href="#"><i class="fa fa-instagram"></i> @ulethbridge </li></a>
+					<li><a href="#"><i class="fa fa-linkedin-square"></i> ulethbridge </li></a>
+					
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<ul>
+					<li><strong>Campus</strong></li>
+					<li><a href="#"> Directory </li></a>
+					<li><a href="#"> Careers </li></a>
+					<li><a href="#"> News &amp; Events </li></a>
+					<li><a href="#"> Library </li></a>
+					<li><a href="#"> Map &amp; Directions </li></a>
+					<li><a href="#"> Bookstore </li></a>
+					<li><a href="#"> Sport &amp; Recreation </li></a>
+					
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<ul>
+					<li><strong>Policies</strong></li>
+					<li><a href="#">Privacy Policy </li></a>
+					</ul>
+					<ul>
+					<li><strong>University Initiatives</strong></li>
+					<li><a href="#">Destination 2020 </li></a>
+					<li><a href="#">50<sup>th</sup> Anniversary </li></a>
+					<li><a href="#">Supporting our Students </li></a>
+					
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="util-footer" class="container-fluid">
+		<div  class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					&copy; 2017 University of Lethbridge | 4401 University Drive, Lethbridge, Alberta T1K 3M4 | Phone: (403) 329-2111
+				</div>
+			</div>
+		</div>
+	</div>
+		
+</div>
